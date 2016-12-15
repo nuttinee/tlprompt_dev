1ea754e0c6a39230e9213a0c6c565742c30dd2ef
nuttinee <nuttinee@thailife.com>
2016-12-15 14:00:56 +0700

    Add file to git

---
+++ assets/js/function.js
@@ -0,0 +1,18 @@
+/*
+// popup insurance-cal
+$.fancybox({
+	href:'ajax/popup-insurance-cal.php',
+	type:'ajax',
+	padding:0,
+	margin:0, 
+	fitToView : false, 
+	modal:true
+});
+$.fancybox({
+	href:'ajax/popup-price.php',
+	type:'ajax',
+	padding:0,
+	margin:0, 
+	fitToView : false
+});
+*/
